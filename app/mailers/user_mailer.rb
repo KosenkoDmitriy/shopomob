73810d3645e41b1e4b36d53204aaa8e4c392bb41
dmitry <kosenkodmitryv@gmail.com>
2014-10-12 21:31:47 +0400

    init commit shopomob project

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ActionMailer::Base
+  #default from: "shopomob@shopomob.ru"
+  default from: "kosenkodmitryv@gmail.com"
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: "Добро пожаловать!")
+  end
+end
