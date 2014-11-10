9ec001dc6147d6b770abd9cfd3aa3659d86c9bb7
dmitry <kosenkodmitryv@gmail.com>
2014-11-10 19:29:00 +0400

    update services. change size of the logo.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :image, :styles => { :slider=>"1674x550!", :slider_small => "370x193\!", :normal =>"640x640", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/home/adec_logo.png"
+  has_attached_file :image, :styles => { :slider=>"1674x550!", :slider_small => "370x193!", :normal =>"640x640", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/home/adec_logo.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def image_url_thumb
