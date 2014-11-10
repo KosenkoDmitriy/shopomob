701f1f7b7b17134c06fc77df78dff7f489681339
dmitry <kosenkodmitryv@gmail.com>
2014-11-10 09:54:14 +0400

    scale images for gallery and slider. add company location.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/home/shopomob.png"
+  has_attached_file :image, :styles => { :slider=>"1674x550!", :slider_small => "370x193\!", :normal =>"640x640", :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/home/adec_logo.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def image_url_thumb
@@ -12,6 +12,9 @@ class Image < ActiveRecord::Base
   end
   def image_url_original
     image.url(:original)
+  end
+  def image_url_slider
+    image.url(:slider)
   end
 
 end
