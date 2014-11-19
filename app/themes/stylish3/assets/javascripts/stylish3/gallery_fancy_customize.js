b790f7ee298ff90fc0d9e5bf3d98abf48ecdbfec
dmitry <kosenkodmitryv@gmail.com>
2014-11-19 21:57:13 +0400

    share link to gallery

--- app/themes/stylish3/assets/javascripts/stylish3/gallery_fancy_customize.js
+++ app/themes/stylish3/assets/javascripts/stylish3/gallery_fancy_customize.js
@@ -3,7 +3,8 @@ $(".fancybox")
     .fancybox({
         beforeShow: function () {
             var caption = $(this.element).data("caption") ? $(this.element).data("caption") : "";
-            this.title = this.title ? this.title + caption : '<script type="text/javascript" src="//yastatic.net/share/share.js" charset="utf-8"></script><div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="button" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,lj,friendfeed,moikrug,gplus,surfingbird"></div>' + caption;
+            this.title = this.title ? this.title + caption : '<script type="text/javascript" src="//yastatic.net/share/share.js" charset="utf-8"></script><div class="yashare-auto-init" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,lj,friendfeed,moikrug,gplus,surfingbird" data-yashareLink="'+'http://'+window.location.hostname+"#gallery_all"+' data-yashareL10n="ru" data-yashareType="button" data-yashareQuickServices="vkontakte,facebook,twitter,odnoklassniki,moimir,lj,friendfeed,moikrug,gplus,surfingbird"></div>' + caption;
+//            +$(".fancybox>img").attr('src')+
         },
         afterShow: function () {
 //            stButtons.locateElements();
