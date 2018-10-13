12f7ca48a34c2c365e677c76ce6ca4d2e2a72b29
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-10-14 00:57:38 +0300

    Fixed test email settings

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,4 +4,14 @@ class HomeControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "service should return en translation" do
+    I18n.locale = :en
+    Service.create(title: 'en service')
+    I18n.locale = :ru
+    Service.create(title: 'ru service')
+
+    en_services = Service.with_translations(I18n.locale)
+    I18n.locale = :ru
+    assert_equal(en_services.count, 1)  
+  end
 end
