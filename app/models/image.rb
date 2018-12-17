f6df72500c6be0403a34074a3175202d4afdc1f5
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-12-17 18:58:16 +0300

    changed default/no image

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,8 +4,9 @@ class Image < ActiveRecord::Base
 
   belongs_to :imageable, polymorphic: true
 
-  has_attached_file :image, :default_url => "sm/no_image/NoImageAdded.png", 
-  :styles => { :slider=>"1674x550!", :slider_small => "370x193!", :normal =>"640x640", :medium => "300x300>", :small => "200x200>", :thumb => "100x100>" }
+  has_attached_file :image, :default_url => "sm/no_image/NoPhoto.png", 
+    :styles => { :slider=>"1674x550!", :slider_small => "370x193!", :normal =>"640x640", :medium => "300x300>", :small => "200x200>", :thumb => "100x100>" }
+
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def image_url_thumb
