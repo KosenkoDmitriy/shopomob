2c74f0e2cef91accc7ac1432c9937a7e9c5a3793
Dmitry Kosenko <kosenkodmitryv@gmail.com>
2018-12-21 18:27:10 +0300

    fixed ru website version

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -192,7 +192,9 @@ class HomeController < ApplicationController
   private
   def theme_resolver
     #params[:theme].presence || 'stylish3'
-    params[:theme] || I18n.t('theme')
+    # params[:theme] || I18n.t('theme')
+    # params[:theme] || Rails.application.config.theme
+    Rails.application.config.theme
   end
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
